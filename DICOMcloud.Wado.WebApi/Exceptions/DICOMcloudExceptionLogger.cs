7a4c2d881e6763aa4b7930b112cd13a24b18f310
zaid al-safadi <zsafadi@hotmail.com>
2017-08-27 11:38:29 -0400

    add excpetion handler classes to build webapi project

--- DICOMcloud.Wado.WebApi/Exceptions/DICOMcloudExceptionLogger.cs
+++ DICOMcloud.Wado.WebApi/Exceptions/DICOMcloudExceptionLogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http.ExceptionHandling;
-using Microsoft.ApplicationInsights;
+
 
 namespace DICOMcloud.Wado.WebApi.Exceptions
 {
@@ -17,8 +17,6 @@ namespace DICOMcloud.Wado.WebApi.Exceptions
                                                                     context.Exception, 
                                                                     context.Request ) ;
             }
-
-            TelemetryClient telemetry = new TelemetryClient();
         }
     }
 }
