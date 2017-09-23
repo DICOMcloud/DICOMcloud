100c99523afd9054a95923ed3c2bdf8b721565ae
zaid al-safadi <zsafadi@hotmail.com>
2017-09-22 21:15:39 -0400

    update nuget to release build

--- Resources/Build/DICOMcloud.bat
+++ Resources/Build/DICOMcloud.bat
@@ -3,8 +3,8 @@ copy "Default.nuspec" "../../DICOMcloud.Azure\DICOMcloud.Azure.nuspec"
 copy "Default.nuspec" "../../DICOMcloud.DataAccess.Database\DICOMcloud.DataAccess.Database.nuspec"
 copy "Default.nuspec" "../../DICOMcloud.Wado\DICOMcloud.Wado.nuspec"
 
-nuget pack ../../DICOMcloud\DICOMcloud.csproj                                          -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
-nuget pack ../../DICOMcloud.Azure\DICOMcloud.Azure.csproj                              -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
-nuget pack ../../DICOMcloud.DataAccess.Database\DICOMcloud.DataAccess.Database.csproj  -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
-nuget pack ../../DICOMcloud.Wado\DICOMcloud.Wado.csproj                                -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
-nuget pack ../Build\DICOMcloud.Wado.WebApi\DICOMcloud.Wado.WebApi.csproj         -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects -tool -build -verbosity detailed
+nuget pack ../../DICOMcloud\DICOMcloud.csproj -Prop Configuration=Release  -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
+nuget pack ../../DICOMcloud.Azure\DICOMcloud.Azure.csproj -Prop Configuration=Release  -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
+nuget pack ../../DICOMcloud.DataAccess.Database\DICOMcloud.DataAccess.Database.csproj -Prop Configuration=Release  -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
+nuget pack ../../DICOMcloud.Wado\DICOMcloud.Wado.csproj -Prop Configuration=Release -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects
+nuget pack ../Build\DICOMcloud.Wado.WebApi\DICOMcloud.Wado.WebApi.csproj -Prop Configuration=Release  -OutputDirectory ../../dist\NuGet\ -IncludeReferencedProjects -tool -build -verbosity detailed
