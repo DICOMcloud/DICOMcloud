ae4386fb0114272ee22f021be6cfcc1d78ebebaa
Zaid Safadi <zsafadi@hotmail.com>
2017-09-30 19:26:34 -0400

    Add trace logging to application insights

--- DICOMcloud.Wado.WebApi/Exceptions/DICOMcloudExceptionLogger.cs
+++ DICOMcloud.Wado.WebApi/Exceptions/DICOMcloudExceptionLogger.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http.ExceptionHandling;
-
+using Microsoft.ApplicationInsights;
 
 namespace DICOMcloud.Wado.WebApi.Exceptions
 {
     public class DICOMcloudExceptionLogger : ExceptionLogger
     {
+        private readonly TelemetryClient _telemetryClient = new TelemetryClient();
+        
         public override void Log(ExceptionLoggerContext context)
         {
             if (context !=null && context.Exception != null)
             {
-                Dicom.Log.LogManager.GetLogger ( "Global" ).Error ( context.Exception.Message, 
-                                                                    context.Exception, 
-                                                                    context.Request ) ;
+                _telemetryClient.TrackException (context.Exception);
             }
         }
     }
