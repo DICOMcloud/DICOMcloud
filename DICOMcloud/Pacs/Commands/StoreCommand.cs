4b573d52ec4c8aceadadb1a12e21763bb89e354f
Zaid Safadi <zsafadi@hotmail.com>
2018-10-28 09:05:38 -0400

    Support generating JPG pre-cached on store #35

--- DICOMcloud/Pacs/Commands/StoreCommand.cs
+++ DICOMcloud/Pacs/Commands/StoreCommand.cs
@@ -199,8 +199,9 @@ namespace DICOMcloud.Pacs.Commands
             MediaTypes.Add ( new DicomMediaProperties ( MimeMediaTypes.xmlDicom ) ) ;
             MediaTypes.Add(new DicomMediaProperties(MimeMediaTypes.DICOM, DicomTransferSyntax.JPEG2000Lossless.UID.UID));
             MediaTypes.Add(new DicomMediaProperties(MimeMediaTypes.DICOM, DicomTransferSyntax.JPEG2000Lossy.UID.UID));
-            //MediaTypes.Add ( new DicomMediaProperties ( MimeMediaTypes.Jpeg, DicomTransferSyntax.JPEGProcess14SV1.UID.UID ) ) ;
-            //MediaTypes.Add ( new DicomMediaProperties ( MimeMediaTypes.Jpeg, DicomTransferSyntax.JPEGProcess1.UID.UID ) ) ;
+            //MediaTypes.Add ( new DicomMediaProperties ( MimeMediaTypes.DICOM, DicomTransferSyntax.JPEGProcess14SV1.UID.UID ) ) ;
+            //MediaTypes.Add ( new DicomMediaProperties ( MimeMediaTypes.DICOM, DicomTransferSyntax.JPEGProcess1.UID.UID ) ) ;
+            MediaTypes.Add(new DicomMediaProperties(MimeMediaTypes.Jpeg));
         }
 
         public IList<DicomMediaProperties> MediaTypes { get; private set ;}
