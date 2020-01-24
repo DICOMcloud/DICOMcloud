5741df3ac35da5b41031cda90baac423309022d5
nutzlastfan <denny@altglienicker.de>
2020-01-24 10:58:31 +0100

    Update OHIFViewerController.cs

--- DICOMcloud.Wado.WebApi/Controllers/OHIFViewerController.cs
+++ DICOMcloud.Wado.WebApi/Controllers/OHIFViewerController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,11 +18,33 @@ namespace DICOMcloud.Wado.WebApi.Controllers
             OhifService = ohifService;
         }
 
+
+         
+
         [Route("ohif/study/{studyUid}/series")]
         [HttpGet]
         public HttpResponseMessage GetStudy(string studyUid )
         {
             return OhifService.GetStudies (new ObjectId ( ) {StudyInstanceUID = studyUid}) ;
+        }
+
+
+    
+
+        [Route("ohif/study/{studyUid}/series/{seriesUid}/instances")]
+        [HttpGet]
+        public HttpResponseMessage GetSeries(string studyUid, string seriesUid)
+        {
+            return OhifService.GetSeries(new ObjectId( ) {StudyInstanceUID = studyUid, SeriesInstanceUID = seriesUid}, new ObjectId() { StudyInstanceUID = studyUid, SeriesInstanceUID = seriesUid});
+        }
+
+
+       
+        [Route("ohif/study/{studyUid}/series/{seriesUid}/instances/{sopUid}/frames")]
+        [HttpGet]
+        public HttpResponseMessage GetInstances(string studyUid, string seriesUid, string sopUid)
+        {
+            return OhifService.GetInstances(new ObjectId() { StudyInstanceUID = studyUid, SeriesInstanceUID = seriesUid }, new ObjectId() { StudyInstanceUID = studyUid, SeriesInstanceUID = seriesUid }, new ObjectId() { StudyInstanceUID = studyUid, SeriesInstanceUID = seriesUid,SOPInstanceUID = sopUid });
         }
     }
 }
