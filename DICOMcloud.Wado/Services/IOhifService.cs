4325e3f89ede1d96a059459390d8eb1bb1971ab3
nutzlastfan <denny@altglienicker.de>
2020-01-24 16:25:03 +0100

    Update IOhifService.cs
    added interface for serie and instance level

--- DICOMcloud.Wado/Services/IOhifService.cs
+++ DICOMcloud.Wado/Services/IOhifService.cs
@@ -1,9 +1,14 @@
-﻿using System.Net.Http;
+using System.Net.Http;
 
 namespace DICOMcloud.Wado
 {
     public interface IOhifService
     {
         HttpResponseMessage GetStudies(IStudyId studyId);
+
+        HttpResponseMessage GetSeries(IStudyId studyId, ISeriesId seriesId);
+
+        HttpResponseMessage GetInstances(IStudyId studyUid, ISeriesId seriesUid, IObjectId sopUid);
+
     }
 }
