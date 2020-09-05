42986327b79b9836c633da9a167142284ed8f73f
Laredo <laredoza@gmail.com>
2020-09-05 18:12:05 +0200

    #104 Merged changes and replaced CloudConfigurationManager with IConfiguration in AzureStorageService.

--- Docker/docker_test_run.sh
+++ Docker/docker_test_run.sh
@@ -1,2 +1,2 @@
-sudo docker build --rm -t dicom-cloud-api-test ../ -f ../Development.Dockerfile
-sudo docker run --rm -it -p 5021:80 dicom-cloud-api-test
+sudo docker build --rm -t dicomcloud/dicomcloud ../ -f ../Development.Dockerfile
+sudo docker run --rm -it -p 5021:80 dicomcloud/dicomcloud --name dicom-cloud-api-test
